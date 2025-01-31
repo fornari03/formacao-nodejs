2adc1c94ebb1fa4e44849539c4a64afc7477f1a4
fornari03 <221017023@aluno.unb.br>
2025-01-31 20:28:26 -0300

    projeto qr-code: atualiza handle de password e run do prompt para funcionar a implementação de limites dos tipos de caracteres na senha

--- projeto-qrcode/src/services/password/handle.js
+++ projeto-qrcode/src/services/password/handle.js
@@ -1,14 +1,28 @@
-import permittedCharacters from "./utils/permitted-characters.js";
+import * as chars_utils from "./utils/permitted-characters.js";
 
 async function handle() {
   let characters = [];
   let password = "";
 
   const passwordLength = process.env.PASSWORD_LENGTH;
-  characters = await permittedCharacters();
+  characters = chars_utils.permittedCharacters();
+  let quantEach = chars_utils.quantOfEachCharacters();
+  [characters, quantEach] = await Promise.all([characters, quantEach]); // permite que as duas funções executem em paralelo
 
   for (let i = 0; i < passwordLength; i++) {
     const index = Math.floor(Math.random() * characters.length);
+    if (quantEach.uppercase > 0 && chars_utils.uppercaseLetters.includes(characters[index])) {
+      quantEach.uppercase--;
+    } else if (quantEach.lowercase > 0 && chars_utils.lowercaseLetters.includes(characters[index])) {
+      quantEach.lowercase--;
+    } else if (quantEach.numbers > 0 && chars_utils.numbers.includes(characters[index])) {
+      quantEach.numbers--;
+    } else if (quantEach.special > 0 && chars_utils.specialCharacters.includes(characters[index])) {
+      quantEach.special--;
+    } else {
+      i--;
+      continue;
+    }
     password += characters[index];
   }
 
--- projeto-qrcode/src/services/prompt/run.js
+++ projeto-qrcode/src/services/prompt/run.js
@@ -1,17 +1,22 @@
 import createQRCode from "../qr-code/create.js";
-import createPassword from "../password/create.js";
+import {createPassword, allowPasswordCreation} from "../password/create.js";
 import prompt from "prompt";
 import promptSchemaMain from "../../prompts-schema/prompt-schema-main.js";
+import chalk from "chalk";
 
 async function runPromptSchemaMain() {
-    prompt.get(promptSchemaMain, async (err, choose) => {
-        if (err) console.log(err);
-    
-        if (choose.select == 1) await createQRCode();
-        if (choose.select == 2) await createPassword();
-      });
-    
-      prompt.start();
+    if (await allowPasswordCreation()) {
+        prompt.get(promptSchemaMain, async (err, choose) => {
+            if (err) console.log(err);
+        
+            if (choose.select == 1) await createQRCode();
+            if (choose.select == 2) await createPassword();
+        });
+        
+        prompt.start();
+    } else {
+        console.log(chalk.red("The password length is greater than the sum of the characters allowed."));
+    }
 }
 
 export default runPromptSchemaMain;
