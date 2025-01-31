8c3b7d9de488da77cbe0bae0a1ec6dd1670ea526
fornari03 <221017023@aluno.unb.br>
2025-01-31 20:26:44 -0300

    projeto qr-code: implementa limites de cada tipo de caracter na geração da senha

--- projeto-qrcode/src/services/password/create.js
+++ projeto-qrcode/src/services/password/create.js
@@ -1,10 +1,17 @@
 import chalk from "chalk";
 import handle from "./handle.js";
+import { quantOfEachCharacters } from "./utils/permitted-characters.js";
+
+async function allowPasswordCreation() {
+  const quantEach = await quantOfEachCharacters();
+  if (process.env.PASSWORD_LENGTH <= (quantEach.uppercase + quantEach.lowercase + quantEach.numbers + quantEach.special)) return true;
+  return false;
+}
 
 async function createPassword() {
-  console.log(chalk.green("password"));
+  console.log(chalk.green("Password:"));
   const password = await handle();
   console.log(password);
 }
 
-export default createPassword;
+export {createPassword, allowPasswordCreation};
--- projeto-qrcode/src/services/password/utils/permitted-characters.js
+++ projeto-qrcode/src/services/password/utils/permitted-characters.js
@@ -1,19 +1,33 @@
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const specialCharacters = "!@#$%^&*()-_";
+
 //extract method
 async function permittedCharacters() {
   let permitted = [];
 
   if (process.env.UPPERCASE_LETTERS === "true")
-    permitted.push(..."ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    permitted.push(...uppercaseLetters);
 
   if (process.env.LOWERCASE_LETTERS === "true")
-    permitted.push(..."abcdefghijklmnopqrstuvwxyz");
+    permitted.push(...lowercaseLetters);
 
-  if (process.env.NUMBERS === "true") permitted.push(..."0123456789");
+  if (process.env.NUMBERS === "true") permitted.push(...numbers);
 
   if (process.env.SPECIAL_CHARACTERS === "true")
-    permitted.push(..."!@#$%^&*()-_");
+    permitted.push(...specialCharacters);
 
   return permitted;
 }
 
-export default permittedCharacters;
+async function quantOfEachCharacters() {
+  return {
+    uppercase: process.env.UPPERCASE_LETTERS === "true" ? parseInt(process.env.UPPERCASE_LETTERS_LIMIT) : 0,
+    lowercase: process.env.LOWERCASE_LETTERS === "true" ? parseInt(process.env.LOWERCASE_LETTERS_LIMIT) : 0,
+    numbers: process.env.NUMBERS === "true" ? parseInt(process.env.NUMBERS_LIMIT) : 0,
+    special: process.env.SPECIAL_CHARACTERS === "true" ? parseInt(process.env.SPECIAL_CHARACTERS_LIMIT) : 0,
+  };
+}
+
+export { permittedCharacters, quantOfEachCharacters, uppercaseLetters, lowercaseLetters, numbers, specialCharacters };
